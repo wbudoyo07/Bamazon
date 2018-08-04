f54483a0a567931ce76aeab2f66252284692ab6e
William Budoyo <williambu07@gmail.com>
2018-08-04 07:02:54 -0400

    fixed bugs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,10 @@
 const mysql = require("mysql");
 const Table = require('cli-table');
 const inquirer = require("inquirer");
+let totalCost=0;
+// var totalCost=0;
 
-// create an table 
-const table = new Table({
-    head: ['ID', 'Product Name', 'department_name', 'Price','Quantity']
-  , colWidths: [5, 50,50,20,10]
-});
+
 // connect  to the database
 const connection = mysql.createConnection({
      
@@ -21,19 +19,23 @@ const connection = mysql.createConnection({
 connection.connect( error => {
 
     if(error) throw error;
-   console.log("Connection as id" +connection.threadId);
-   main();
+
+   displayTable();
   
 });
 
 
-function main(){
-    displayTable();
-}
+
 // show the results of the items
 function displayTable(){
     
-    // query the data from products TABLE
+    // create an table 
+    const table = new Table({
+    head: ['ID', 'Product Name', 'department_name', 'Price','Quantity']
+    , colWidths: [5, 50,50,20,10]
+    });
+
+    // query the data from products TABLE and push all the data to table NPM
     connection.query("SELECT * FROM products", (error,results) => {
 
         if (error) throw error;
@@ -44,9 +46,12 @@ function displayTable(){
         }
 
         console.log(table.toString());
+        
         promptQuestions();
-    
+        
+        
     });
+    
    
 };
 
@@ -69,33 +74,29 @@ function promptQuestions(){
             } 
         ])
         .then(function(answer){
-            // checkQuantity(answer);
-            checkItemID(answer);
-            // console.log(answer);
+            
+            purchaseItem(answer);
+        });
+}
 
-
- });
-
- }
-
- function checkItemID(userInput){
+ function purchaseItem(userInput){
     
     // query the data from mysql
     connection.query("SELECT * FROM products where ?", {item_id: userInput.itemID}, function(error, results){
-        
+      
         if(error) throw error;
         // check user input 
         if(parseInt(userInput.itemQuantity) > results[0].stock_quantity){
-             console.log(`We don't have enough stock`);
+             console.log(`Insufficient quantity!`);
              
             // purchaseItem(userInput);
-            
+            promptQuestions();
         }else{
             
-            purchaseItem(userInput,results[0].stock_quantity);
+            updateQuantity(userInput,results[0].stock_quantity,results[0].price);
             // console.log(results[0].stock_quantity);
         }
-        promptQuestions();
+       
         //else your id was wrong please try again
         
     });
@@ -103,20 +104,48 @@ function promptQuestions(){
     
  }
 
- function purchaseItem(userInput,stock){
+ function updateQuantity(userInput,stock,price){
+
+    let newItemQuantity =stock - parseInt(userInput.itemQuantity)
      connection.query("UPDATE products SET ? WHERE ?",
      [
          {
-             stock_quantity: stock - parseInt(userInput.itemQuantity)
+             stock_quantity: newItemQuantity
          },
          {
              item_id: userInput.itemID
          }
       ], 
-       function(error,){
+       function(error, results){
 
         if(error) throw error;
-        console.log("you success");
-        promptQuestions();
+        console.log(`Thanks for buying with us`);
+        totalCost += price * parseInt(userInput.itemQuantity);
+        console.log(totalCost)
+         
+        choicesPrompt();
      });
- }
+}
+
+function choicesPrompt(){
+    inquirer
+        .prompt({
+            name:"newTransaction",
+            type:"list",
+            message:"Do you wish to another stuff ?",
+            choices:["YES","NO"]
+        })
+        .then(function(answer){
+            if(answer.newTransaction === "YES"){
+                displayTable();
+            }
+            else{
+                console.log("Thanks for having service with BAMAZON");
+            }
+        });
+}
+// we need to find the item that customer choices
+// get the price of customer of choices
+// assign all the price to variable and add them up
+// print the customer total price
+
